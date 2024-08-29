65c900e2d7454d04a8837711d837f8f2a0bf2e95
Raiyan <raiyan.appdid@gmail.com>
2024-08-29 09:55:11 +0530

    updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Nunito } from 'next/font/google'
 import '@/app/global.css'
 import Navigation from './(auth)/Navigation'
+import NextTopLoader from 'nextjs-toploader'
 
 const nunitoFont = Nunito({
     subsets: ['latin'],
@@ -12,6 +13,21 @@ const RootLayout = ({ children }) => {
         <html lang="en" className={nunitoFont.className}>
             <body className="antialiased">
                 <Navigation />
+                <NextTopLoader
+                    color="#2299DD"
+                    initialPosition={0.08}
+                    crawlSpeed={200}
+                    height={3}
+                    crawl={true}
+                    showSpinner={false}
+                    easing="ease"
+                    speed={200}
+                    shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+                    template='<div class="bar" role="bar"><div class="peg"></div></div> 
+                        <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+                    zIndex={1600}
+                    showAtBottom={false}
+                />
                 {children}
             </body>
         </html>
